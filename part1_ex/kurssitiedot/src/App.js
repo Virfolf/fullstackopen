324e4f864f3a9ec183b5260a2a4e94211c2f5cd7
Tommi Kumlin <tommi.kumlin@foxcode.plus>
2023-08-01 14:54:20 +0300

    Part 1b done.

--- part1_ex/kurssitiedot/src/App.js
+++ part1_ex/kurssitiedot/src/App.js
@@ -1,41 +1,59 @@
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
+const Content = (props) => {
+  console.log(props)
+  const results = []
+  return (  
+    <div>
+      {props.course.parts.forEach(value => 
+        results.push(
+          <p key={value.name}>
+            {value.name} {value.exercises}
+          </p>
+        )
+      )}
+      {results}
+    </div>
+
   )
 }
 
 const Total = (props) => {
   return (
     <p>
-      <p>Number of exercises {props.exercises.reduce((sum, i) => sum = sum + i , 0)}</p>
+      Number of exercises {props.course.parts.reduce((sum, value) => sum = sum + value.exercises, 0)}
     </p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course}/>
-      <Part part={part1} exercises={exercises1}/>
-      <Part part={part2} exercises={exercises2}/>
-      <Part part={part3} exercises={exercises3}/>
-      <Total exercises={[exercises1,exercises2,exercises3]}/>
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
 }
